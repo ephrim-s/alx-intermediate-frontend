92cb020d7f9b1cb717904dcbe1ff83217ec5324e
ephrim-s <ephrims5@gmail.com>
2025-07-03 00:59:51 +0000

    tailwind css updated

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./1-index.html",
+    "./2-index.html",
+  ],
   theme: {
     extend: {},
   },
